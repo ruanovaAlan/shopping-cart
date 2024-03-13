794076c22a7a39813e4a221ef760af69e883e36f
Alan Ruanova <ruanovaalan96@gmail.com>
2024-03-12 19:14:13 -0600

    Add onChange prop to Filters component

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
 import './Filters.css'
 
-export function Filters() {
+export function Filters({ onChange }) {
   const [minPrice, setMinPrice] = useState(0)
 
   const handleChangeMinPrice = (event) => {
     setMinPrice(event.target.value)
+    onChange(prevState => ({
+      ...prevState,
+      minPrice: event.target.value
+    }))
+  }
+
+  const handleChangeCategory = (event) => {
+    onChange(prevState => ({
+      ...prevState,
+      category: event.target.value
+    }))
   }
 
   return (
@@ -25,7 +36,7 @@ export function Filters() {
 
       <div>
         <label htmlFor='category'>Categoría</label>
-        <select id='category'>
+        <select id='category' onChange={handleChangeCategory}>
           <option value='all'>Todas</option>
           <option value='laptops'>Laptops</option>
           <option value='smartphones'>Celulares</option>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import { Filters } from './Filters'
 
-export function Header() {
+export function Header({ changeFilters }) {
   return (
     <header>
       <h1>React Shop🛒</h1>
-      <Filters />
+      <Filters onChange={changeFilters} />
     </header>
   )
 }
